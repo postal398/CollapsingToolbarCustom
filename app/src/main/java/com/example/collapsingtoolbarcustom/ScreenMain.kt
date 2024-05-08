d91ce21bfcb8831bd033e8cefd1feb64138bb848
Vadim <maslovvadim398@gmail.com>
2024-05-08 14:55:53 +0300

    start to do backsize of header on any up move, 52-60 in ScreenMain file

--- app/src/main/java/com/example/collapsingtoolbarcustom/ScreenMain.kt
+++ app/src/main/java/com/example/collapsingtoolbarcustom/ScreenMain.kt
@@ -4,12 +4,9 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
-import androidx.compose.material.*
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.CardElevation
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
